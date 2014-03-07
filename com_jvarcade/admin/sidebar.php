32e11854448db4f0121d0688c72506779ff6a0e2
iskar <iskar.enev@gmail.com>
2014-03-07 11:29:32 +0200

    jvarcadeToolbarHelper::addSubmenu should be defined as static method

--- com_jvarcade/admin/sidebar.php
+++ com_jvarcade/admin/sidebar.php
@@ -13,67 +13,37 @@
 defined('_JEXEC') or die('Restricted access');
 class jvarcadeToolbarHelper {
 	
-	function addSubmenu($vName = 'cpanel') {
+	public static function addSubmenu($vName = 'cpanel') {
 	
 		JHtmlSidebar::addEntry(
-		JText::_('COM_JVARCADE_CPANEL'),
-		'index.php?option=com_jvarcade&task=cpanel',
-		$vName == 'cpanel'
-				);
+		JText::_('COM_JVARCADE_CPANEL'), 'index.php?option=com_jvarcade&task=cpanel', $vName == 'cpanel');
 		
 		JHtmlSidebar::addEntry(
-		JText::_('COM_JVARCADE_SETTINGS'),
-		'index.php?option=com_jvarcade&task=settings',
-		$vName == 'settings'
-				);
+		JText::_('COM_JVARCADE_SETTINGS'), 'index.php?option=com_jvarcade&task=settings', $vName == 'settings');
 		
 		JHtmlSidebar::addEntry(
-		JText::_('COM_JVARCADE_MANAGE_SCORES'),
-		'index.php?option=com_jvarcade&task=manage_scores',
-		$vName == 'manage_scores'
-				);
+		JText::_('COM_JVARCADE_MANAGE_SCORES'), 'index.php?option=com_jvarcade&task=manage_scores', $vName == 'manage_scores');
 		
 		JHtmlSidebar::addEntry(
-		JText::_('COM_JVARCADE_MANAGE_FOLDERS'),
-		'index.php?option=com_jvarcade&task=manage_folders',
-		$vName == 'manage_folders'
-				);
+		JText::_('COM_JVARCADE_MANAGE_FOLDERS'), 'index.php?option=com_jvarcade&task=manage_folders', $vName == 'manage_folders');
 		
 		JHtmlSidebar::addEntry(
-		JText::_('COM_JVARCADE_MANAGE_GAMES'),
-		'index.php?option=com_jvarcade&task=manage_games',
-		$vName == 'manage_games'
-				);
+		JText::_('COM_JVARCADE_MANAGE_GAMES'), 'index.php?option=com_jvarcade&task=manage_games', $vName == 'manage_games');
 		
 		JHtmlSidebar::addEntry(
-		JText::_('COM_JVARCADE_UPLOAD_ARCHIVE'),
-		'index.php?option=com_jvarcade&task=upload_archive',
-		$vName == 'upload_archive'
-				);
+		JText::_('COM_JVARCADE_UPLOAD_ARCHIVE'), 'index.php?option=com_jvarcade&task=upload_archive', $vName == 'upload_archive');
 		
 		JHtmlSidebar::addEntry(
-		JText::_('COM_JVARCADE_MAINTENANCE'),
-		'index.php?option=com_jvarcade&task=maintenance',
-		$vName == 'maintenance'
-				);
+		JText::_('COM_JVARCADE_MAINTENANCE'), 'index.php?option=com_jvarcade&task=maintenance', $vName == 'maintenance');
 		
 		JHtmlSidebar::addEntry(
-		JText::_('COM_JVARCADE_CONTENT_RATINGS'),
-		'index.php?option=com_jvarcade&task=content_ratings',
-		$vName == 'content_ratings'
-				);
+		JText::_('COM_JVARCADE_CONTENT_RATINGS'), 'index.php?option=com_jvarcade&task=content_ratings', $vName == 'content_ratings');
 		
 		JHtmlSidebar::addEntry(
-		JText::_('COM_JVARCADE_CONTESTS'),
-		'index.php?option=com_jvarcade&task=contests',
-		$vName == 'contests'
-				);
+		JText::_('COM_JVARCADE_CONTESTS'), 'index.php?option=com_jvarcade&task=contests', $vName == 'contests');
 		
 		JHtmlSidebar::addEntry(
-		JText::_('COM_JVARCADE_SUPPORT'),
-		'http://www.jvitals.com/support/support-forum/default-forum/14-jvarcade.html',
-		$vName == 'support'
-				);
+		JText::_('COM_JVARCADE_SUPPORT'), 'http://www.jvitals.com/support/support-forum/default-forum/14-jvarcade.html', $vName == 'support');
 	
 	}
 	
